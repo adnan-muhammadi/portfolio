6273a2e00c61e3646c84345bd74a459d2b6c5ded
rizwan <rsaleem@macrosoftinc.com>
2022-10-29 16:23:47 +0500

    Final Version

---
+++ constants/data.js
@@ -0,0 +1,100 @@
+const userData = {
+  githubUsername: "adnan-muhammadi",
+  name: "Muhammad Adnan Saleem",
+  designation: "Frontend Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "adnan.jmuhammadi@outlook.com",
+  phone: "+92 3177912895",
+  address: "Karachi, Sindh, Pakistan.",
+  projects: [
+    {
+      title: "Toptech TMS",
+      link: "https://dev-tf.toptechc1.com/",
+      imgUrl: "/tmk1.png",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Frontend developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Toptech TMS",
+    currentProjectUrl: "https://dev-tf.toptechc1.com/",
+  },
+  experience: [
+    {
+      title: "Frontend Developer",
+      company: "ChaiOne",
+      year: "2022",
+      companyLink: "https://chaione.com",
+      desc: "Contributed to our flagship Toptech Terminal Management System",
+    },
+    {
+      title: "Frontend Developer",
+      company: "Techsurge",
+      year: "2017",
+      companyLink: "https://techsurgeinc.com/",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Valley Labs",
+      year: "2017",
+      companyLink: "https://www.indusvalleylabs.com/",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "IQRA University",
+      year: "2017",
+      companyLink: "https://iqra.edu.pk/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 3.3. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "DJ Science College, Karachi.",
+      year: "2014",
+      companyLink: "https://www.facebook.com/dayaramjaethmalcollege/",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1iqLcG0EfArT3kmRjY4F-KyPSQNHNvMbp/view?usp=drivesdk",
+  socialLinks: {
+    instagram: "https://instagram.com/adnan.muhammadi",
+    twitter: "https://twitter.com/adnan_muhamadi",
+    linkedin: "https://linkedin.com/in/adnan-muhammadi",
+    github: "https://github.com/adnan-muhammadi",
+    facebook: "https://facebook.com/adnan.joiya.muhammadi",
+  },
+};
+
+export default userData;
