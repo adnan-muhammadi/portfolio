0540bd5c4db0fa53909e1c8a4a423e193d191702
adnan-muhammadi <--global>
2022-10-29 17:08:53 +0500

    Update

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -35,7 +35,7 @@ export default function FavouriteProjects() {
         <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
           {/* Single card */}
           <a
-            href="https://tailwindmasterkit.com"
+            href="https://dev-tf.toptechc1.com/"
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
@@ -74,7 +74,7 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://manuarora.in"
+            href="https://adnan-three.vercel.app/"
             className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
             <div className="relative overflow-hidden shadow-2xl">
